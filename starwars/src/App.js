f5e602dffd51c675c8099d8ac7408e7f327a4c74
zchristy <zchristy44@gmail.com>
2019-04-26 16:16:15 -0500

    Added music toggle button

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -74,6 +74,19 @@ class App extends Component {
     }
   };
 
+  audioCLick = (event) => {
+    console.log(event.target);
+    let audioButton = event.target;
+    console.log(audioButton);
+    if (audioButton.classList[1] === 'fa-volume-up'){
+      audioButton.classList.remove('fa-volume-up');
+      audioButton.classList.add('fa-volume-mute');
+    } else if (audioButton.classList[1] === 'fa-volume-mute'){
+      audioButton.classList.remove('fa-volume-mute');
+      audioButton.classList.add('fa-volume-up');
+    }
+  }
+
   click = event => {
     // if next button is clicked
     if(event.target.getAttribute('name') === 'nextbutton') {
@@ -87,7 +100,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header" >React Wars<i id='audioBtn' className="fas fa-volume-up" onClick={this.audioCLick}></i></h1>
         <Sound
             url="song.mp3"
             autoLoad={true}
